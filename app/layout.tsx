01c460dc38f30ee5b1d92896a54e65e6542cf8de
lvl100gengar <lvl100gengar@github.com>
2026-04-01 21:59:34 -0400

    Add metadata for dashboard, invoices, and customers pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,18 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+// Exporting metadata from any layout or page will add the appropriate <meta> HTML elements
+// Useful for SEO and sharing on social media sites
+// The %s below is replaced when title metadata is provided on any sub-page/layout
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
